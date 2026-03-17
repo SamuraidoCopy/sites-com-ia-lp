7ef4716ab6f7eba7ec189482f57a18951b9af791
Vitor <vitor@example.com>
2026-03-17 12:21:51 -0300

    chore: configure next.config.ts for github pages export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/sites-com-ia-lp',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
